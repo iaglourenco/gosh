6e4a4ec3d4efb70aa1f068f00834d2f2b9e2ae85
Enzofmcs <enzo.fmcs@gmail.com>
2024-05-21 14:25:26 -0300

    paralelismo
    rodando duas funções separadas por &

--- shell_functions.c
+++ shell_functions.c
@@ -69,8 +69,23 @@ char *search_executable(char *command)
     return NULL; // Retorna NULL se não encontrado
 }
 
-void execute_command(char *command)
-{
+// Função auxiliar para dividir a linha de comando em subcomandos
+char** split_command(char* command, const char* delimiter, int* count) {
+    char** commands = malloc(64 * sizeof(char*)); // ajusta conforme necessário
+    char* token = strtok(command, delimiter);
+    int i = 0;
+
+    while (token != NULL) {
+        commands[i++] = strdup(token);
+        token = strtok(NULL, delimiter);
+    }
+    commands[i] = NULL;
+    *count = i;
+
+    return commands;
+}
+
+void execute_single_command(char *command) {
     char *args[64];
     int arg_count = 0;
     pid_t pid;
@@ -79,38 +94,27 @@ void execute_command(char *command)
     const char *delimiters = " \t\n";
 
     token = strtok(command, delimiters);
-    while (token != NULL && arg_count < 64)
-    {
+    while (token != NULL && arg_count < 64) {
         args[arg_count++] = token;
         token = strtok(NULL, delimiters);
     }
     args[arg_count] = NULL;
 
     // Comandos internos
-    if (strcmp(args[0], "exit") == 0)
-    {
+    if (strcmp(args[0], "exit") == 0) {
         exit(EXIT_SUCCESS);
-    }
-    else if (strcmp(args[0], "cd") == 0)
-    {
-        if (arg_count > 1)
-        {
-            if (chdir(args[1]) != 0)
-            {
+    } else if (strcmp(args[0], "cd") == 0) {
+        if (arg_count > 1) {
+            if (chdir(args[1]) != 0) {
                 print_error(CD_FAILED);
             }
-        }
-        else
-        {
+        } else {
             printf("Uso: cd <caminho>\n");
         }
         return;
-    }
-    else if (strcmp(args[0], "path") == 0)
-    {
+    } else if (strcmp(args[0], "path") == 0) {
         // Define os caminhos especificados
-        for (int i = 1; i < arg_count; i++)
-        {
+        for (int i = 1; i < arg_count; i++) {
             add_path(args[i]);
         }
         return;
@@ -118,45 +122,51 @@ void execute_command(char *command)
 
     // Comando externo
     char *executable_path = search_executable(args[0]);
-    if (executable_path != NULL)
-    {
+    if (executable_path != NULL) {
         pid = fork();
-        if (pid < 0)
-        {
+        if (pid < 0) {
             perror("fork");
             return;
-        }
-        else if (pid == 0)
-        {
-            if (execv(executable_path, args) == -1)
-            {
+        } else if (pid == 0) {
+            if (execv(executable_path, args) == -1) {
                 print_error(EXEC_FAILED);
                 exit(EXIT_FAILURE);
             }
-        }
-        else
-        {
+        } else {
             int status;
             waitpid(pid, &status, 0);
         }
         free(executable_path);
-    }
-    else
-    {
+    } else {
         print_error(INVALID_COMMAND);
     }
+}
 
-    // pid = fork();
-    // if (pid < 0) {
-    //     perror("fork");
-    //     exit(EXIT_FAILURE);
-    // } else if (pid == 0) { // Processo filho
-    //     if (execvp(args[0], args) == -1) {
-    //         print_error(EXEC_FAILED);
-    //         exit(EXIT_FAILURE);
-    //     }
-    // } else { // Processo pai
-    //     int status;
-    //     waitpid(pid, &status, 0);
-    // }
+void execute_command(char *command) {
+    int count;
+    char** commands = split_command(command, "&", &count);
+
+    for (int i = 0; i < count; i++) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Processo filho executa o comando
+            execute_single_command(commands[i]);
+            exit(EXIT_SUCCESS); // Certifique-se de sair após a execução do comando
+        }
+    }
+
+    // Processo pai espera todos os filhos terminarem
+    for (int i = 0; i < count; i++) {
+        int status;
+        wait(&status);
+    }
+
+    // Liberar memória
+    for (int i = 0; i < count; i++) {
+        free(commands[i]);
+    }
+    free(commands);
 }
